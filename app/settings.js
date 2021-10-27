e7249409e83143f0872feaacfb69b79e2cfd524c
Garrett <gflynn@usc.edu>
2021-10-27 15:52:43 -0700

    Updated

--- app/settings.js
+++ app/settings.js
@@ -1,4 +1,80 @@
-import {Parser} from "./Parser.js"
 
-        
-        export const settings = {"name":"Neurofeedback Template","devices":["EEG"],"author":"Garrett Flynn","description":"Get started building a neurofeedback app!","categories":["learn"],"instructions":"Coming soon...","display":{"production":false,"development":true},"intro":{"title":false},"graphs":[{"nodes":[{"name":"eeg","class":brainsatplay.plugins.biosignals.EEG,"style":"transform: scale(0.5); top: 18%; left: 20%;"},{"name":"neurofeedback","class":brainsatplay.plugins.algorithms.Neurofeedback,"params":{},"style":"transform: scale(0.5); top: 44.6667%; left: 20%;"},{"name":"brainstorm","class":brainsatplay.plugins.networking.Brainstorm,"params":{"onUserConnected":"(u) => {\n            let parser = settings.graphs[0].nodes.find(n => n.name === 'parser')\n            parser.instance._userAdded(u)\n          }","onUserDisconnected":"(u) => {\n            let parser = settings.graphs[0].nodes.find(n => n.name === 'parser')\n            parser.instance._userRemoved(u)\n          }"},"style":"transform: scale(0.5); top: 71.3333%; left: 20%;"},{"name":"parser","class":Parser,"params":{},"style":"transform: scale(0.5); top: 18%; left: 46.6667%;"},{"name":"ui","class":brainsatplay.plugins.interfaces.DOM,"params":{"style":"\n          .brainsatplay-ui-container {\n            width: 100%;\n            height: 100%;\n          }\n\n          #content {\n            width: 100%;\n            height: 100%;\n            display: flex;\n            align-items: center;\n            justify-content: center;\n          }\n          "},"style":"transform: scale(0.5) translate3d(242px, -164px, 0px); top: 44.6667%; left: 46.6667%;"}],"edges":[{"source":"eeg:atlas","target":"neurofeedback"},{"source":"neurofeedback","target":"brainstorm"},{"source":"brainstorm:neurofeedback","target":"parser"},{"source":"parser:element","target":"ui:content"}]}],"version":"0.0.37","image":null,"connect":true};
+import {Parser} from './Parser'
+
+export const settings = {
+    name: "Multiplayer",
+    devices: ["EEG"],
+    author: "Garrett Flynn",
+    description: "Get started building a multiplayer neurofeedback app!",
+    categories: ["learn", 'templates'],
+    instructions:"Coming soon...",
+    display: {
+      production: true,
+      development: true
+    },
+
+    intro: {
+      title:false
+    },
+
+    // App Logic
+    graphs:
+    [{
+      nodes: [
+        {name: 'eeg', class: brainsatplay.plugins.biosignals.EEG},
+        {name: 'neurofeedback', class: brainsatplay.plugins.algorithms.Neurofeedback, params: {}},
+        {name: 'brainstorm', class: brainsatplay.plugins.networking.Brainstorm, params: {
+
+          onUserConnected: (u) => {
+            let parser = settings.graphs[0].nodes.find(n => n.name === 'parser')
+            parser.instance._userAdded(u)
+          },
+      
+          onUserDisconnected: (u) => {
+            let parser = settings.graphs[0].nodes.find(n => n.name === 'parser')
+            parser.instance._userRemoved(u)
+          },
+
+        }},
+        {name: 'parser', class: Parser, params: {}},
+        {name: 'ui', class: brainsatplay.plugins.interfaces.DOM, params: {
+          style: `
+          .brainsatplay-ui-container {
+            width: 100%;
+            height: 100%;
+          }
+
+          #content {
+            width: 100%;
+            height: 100%;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+          }
+          `
+        }},
+      ],
+
+      edges: [
+        {
+          source: 'eeg:atlas', 
+          target: 'neurofeedback'
+        },
+        { 
+          source: 'neurofeedback', 
+          target: 'brainstorm'
+        },
+        {
+          source: 'brainstorm:neurofeedback', 
+          target: 'parser'
+        },
+        {
+          source: 'parser:element', 
+          target: 'ui:content'
+        },
+      ]
+    }],
+
+    version: '0.0.37',
+    connect: true
+}
